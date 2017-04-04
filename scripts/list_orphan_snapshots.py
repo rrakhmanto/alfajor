47f1bdf95a412c93f5e5464ce1edf81a3ffe646c
Riko <r.rakhmanto@base2services.com>
2017-04-04 14:45:17 +1000

    debug

--- scripts/list_orphan_snapshots.py
+++ scripts/list_orphan_snapshots.py
@@ -47,7 +47,7 @@ for snapshot in all_snapshots:
 
   if len(amiIdResult) != 1: #check if more than one associated AMI (impossible) or no associated at all.
     volIdResult = reVol.findall(snapshot.description) #find associated volumes
-    print "VolumeId found with snapshot " % snapshotId % " is " % volIdResult
+    print "VolumeId found with snapshot " + snapshotId + " is " + volIdResult
 
     if len(volIdResult) != 1:
       snapshots_no_info[snapshotId] = {"start_time" : snapshot.start_time}
